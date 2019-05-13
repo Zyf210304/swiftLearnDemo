acd2e0031b2d5580cbb5c5b077987e7e7cd27f3b
Zyf210304 <672233580@qq.com>
2019-05-13 22:08:38 +0800

    亚飞
    cell

---
+++ swiftLearnDemo/Main/View/CollectionPrettyCell.swift
@@ -0,0 +1,18 @@
+//
+//  CollectionPrettyCell.swift
+//  swiftLearnDemo
+//
+//  Created by 张亚飞 on 2019/5/13.
+//  Copyright © 2019 张亚飞. All rights reserved.
+//
+
+import UIKit
+
+class CollectionPrettyCell: CollectionBaseCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
